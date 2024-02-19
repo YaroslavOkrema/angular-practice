e43b70952fbb12f2fa7f734ab914a3b8a0a9689d
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-02-19 17:18:24 +0200

    feat: added components + routes

---
+++ practice/src/app/components/transactions-form/transactions-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-form',
+  templateUrl: './transactions-form.component.html',
+  styleUrls: ['./transactions-form.component.scss']
+})
+export class TransactionsFormComponent {
+
+}
